6a517366beea642eb205c1910757db7135bcbc8a
CAIO <caioeduardosantos7@gmail.com>
2022-08-20 16:53:16 -0300

    implementation exception

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import bordgame.Board;
+import bordgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -9,6 +12,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -19,6 +23,14 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(1, 1));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 2));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(3, 1));
+		
 	}
 	
 	
